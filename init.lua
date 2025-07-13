9d2537b6122d53fa4253fb691f91078d83bc8926
Adam Nielsen <adamnielsen401@gmail.com>
2025-07-13 20:16:24 +0200

    added a floating terminal

--- init.lua
+++ init.lua
@@ -5,3 +5,8 @@
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
+
+-- TODO:
+-- [ ] lägga till session memory so i can enter where i left
+-- [X] fixa så jag enkelt kan öppna en terminal
+--      [ ] kankse göra så att man kan öppna ny terminal också beroende på hur lätt det är
