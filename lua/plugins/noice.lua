52224bfef30bfb1888f2bb871dd463e3ccfb4e5d
Adamniels <adamnielsen401@gmail.com>
2025-10-28 08:32:34 +0100

    changed to tranparent background

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -189,7 +189,7 @@ return {
         {
             "rcarriga/nvim-notify",
             opts = {
-                background_colour = "#000000",
+                background_colour = "NONE",
                 stages = "fade_in_slide_out",
                 render = "compact",
             }
