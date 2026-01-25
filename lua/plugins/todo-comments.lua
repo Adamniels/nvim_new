a48823b9ad1a14e0ce1a45d8c544395f63297348
Adamniels <adamnielsen401@gmail.com>
2026-01-25 16:05:55 +0100

    added a new todo comment

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,33 +1,34 @@
 return {
-	"folke/todo-comments.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local todo_comments = require("todo-comments")
+    "folke/todo-comments.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local todo_comments = require("todo-comments")
 
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
+        -- set keymaps
+        local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "]t", function()
-			todo_comments.jump_next()
-		end, { desc = "Next todo comment" })
+        keymap.set("n", "]t", function()
+            todo_comments.jump_next()
+        end, { desc = "Next todo comment" })
 
-		keymap.set("n", "[t", function()
-			todo_comments.jump_prev()
-		end, { desc = "Previous todo comment" })
+        keymap.set("n", "[t", function()
+            todo_comments.jump_prev()
+        end, { desc = "Previous todo comment" })
 
-		-- setup with custom keyword "REDOVISA"
-		todo_comments.setup({
-			keywords = {
-				TODO = { icon = " ", color = "info" },
-				FIXME = { icon = " ", color = "error" },
-				HACK = { icon = " ", color = "warning" },
-				WARN = { icon = " ", color = "warning" },
-				PERF = { icon = " ", color = "hint" },
-				NOTE = { icon = " ", color = "hint" },
-				-- Lägg till din egen nyckel här
-				REDOVISA = { icon = " ", color = "error", alt = { "REPORT" } },
-			},
-		})
-	end,
+        -- setup with custom keyword "REDOVISA"
+        todo_comments.setup({
+            keywords = {
+                TODO = { icon = " ", color = "info" },
+                FIXME = { icon = " ", color = "error" },
+                HACK = { icon = " ", color = "warning" },
+                WARN = { icon = " ", color = "warning" },
+                PERF = { icon = " ", color = "hint" },
+                NOTE = { icon = " ", color = "hint" },
+                -- Lägg till din egen nyckel här
+                REDOVISA = { icon = " ", color = "error", alt = { "REPORT" } },
+                PLAN = { icon = " ", color = "info" },
+            },
+        })
+    end,
 }
